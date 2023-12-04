6aad67973ad786b8a0310e6145c0f1df1a103f84
Larry Xue <61249260+leqingxue@users.noreply.github.com>
2023-12-04 07:05:59 -0600

    Update algo.py
    Added two directions using the math library to calculate x and y

--- rws-webapp/algo.py
+++ rws-webapp/algo.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 import numpy as np
+import math
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -15,10 +16,13 @@ model4 = joblib.load('models/finalized_model_weeds.sav')
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
+    direction_radians = math.radians(data['direction'])
 
     # Extract features in the order expected by the models
     features = [
-        data['direction'],
+        math.cos(direction_radians),
+        math.sin(direction_radians),
+        data['speed'],
         data['humidity'],
         data['temp'],
         data['pressure'],
@@ -36,10 +40,10 @@ def predict():
 
     # Return all predictions
     return jsonify({
-        "prediction1": prediction1.tolist(),
-        "prediction2": prediction2.tolist(),
-        "prediction3": prediction3.tolist(),
-        "prediction4": prediction4.tolist()
+        "Cedar": prediction1.tolist(),
+        "Fall Elm": prediction2.tolist(),
+        "Ragweed": prediction3.tolist(),
+        "Weeds": prediction4.tolist()
     })
 
 if __name__ == '__main__':
